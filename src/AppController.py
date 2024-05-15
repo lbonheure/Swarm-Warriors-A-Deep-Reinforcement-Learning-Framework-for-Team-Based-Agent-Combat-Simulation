261c18d6459f4caa2e147a4dd0b470b0b5795255
Luka Bonheure <Luka.Bonheure@ulb.be>
2024-05-15 11:56:33 +0200

    walls + ore + auto run

--- src/AppController.py
+++ src/AppController.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import random
+import threading as thr
+import time
 
 from AppView import AppView
 
@@ -7,8 +9,10 @@ class AppController(AppView.Listener):
     def __init__(self) -> None:
         self.appView = AppView()
         self.appView.setListener(self)
-        self.grid = self.appView.createGrid(20, 20)
-        self.agents = {"agent1": (3,3,"blue"), "agent2": (5,10,"green")}
+        self.grid = self.appView.createGrid(random=True)
+        self.agents = {"agent1": (0,0,"blue"), "agent2": (19,19,"green")}
+        self.running = False
+        self.speed_simu = self.appView.get_speed_simu()
 
     def run(self):
         self.appView.show()
@@ -47,3 +51,18 @@ class AppController(AppView.Listener):
             self.agents[a] = (x, y, c)
         
         self.test_show_agents()
+
+
+    def new_map(self):
+        self.grid.reset_map()
+        
+        
+    def run_simu(self):
+        self.running = True
+        while self.running:
+            self.random_move()
+            time.sleep(1/self.speed_simu.get())
+            
+            
+    def stop_simu(self):
+        self.running = False
